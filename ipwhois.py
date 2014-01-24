f6be797169e080a7d48ef43f56a50b6f52c1d35e
L0j1k <L0j1k@L0j1k.com>
2014-01-24 16:41:23 -0600

    added stuff and things and removed cruft

--- ipwhois.py
+++ ipwhois.py
@@ -9,6 +9,11 @@
 import re, socket, sys
 
 version = '0.1.0a'
+
+def sysprint(data):
+  sys.stdout.write(data)
+  sys.stdout.flush()
+
 def usage(message):
   print(sys.argv[0],version,"by L0j1k")
   print("checks a file for ip addresses and checks whois for each")
@@ -25,5 +30,11 @@ print("[+] checking file",inputFile,"...")
 
 with open(inputFile, 'r') as input:
   for line in input:
-    if(line.find() != 1):
-      print("[=]",line)
+    line = line.rstrip()
+    sysprint("[=] "+line)
+    for chunk in line.split():
+      sysprint("[-] "+chunk+"\n")
+      matchez = re.search('([0123]?\.[0-9]?\.[0-9]+):()', chunk)
+      if(matchez.group(1)):
+        sysprint(matchez.group(0))
+    sysprint("\n")
