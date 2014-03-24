375a54f0af40325d1869f18e3b1244dbc34b1ef2
Sasha Hart <s@sashahart.net>
2014-03-24 14:52:43 -0500

    create main(), push parser creation into own function

--- optool.py
+++ optool.py
@@ -165,180 +165,203 @@ def usage():
     print("")
     sys.exit(0)
 
-parser = argparse.ArgumentParser(
-    description="Perform a variety of byte-level operations on files or byte sequences.",
-    prog="optool.py",
-    usage="optool.py"
-)
-# here goes [OPTIONS] you want to feed to your command
-parser.add_argument("--version",
-    action='version',
-    version='optool.py ' + app_version + ' by L0j1k'
-)
-#parser.set_defaults(func=main)
-subparsers = parser.add_subparsers(help="sub-command help")
-# extract subparser
-parser_extract = subparsers.add_parser("extract",
-    help="extract a segment of specified length from specified offset in target file"
-)
-parser_extract.add_argument("-a", "--address",
-    help="(hex) base hexadecimal address for extraction",
-    const=0,
-    default=0,
-    metavar='hex_address',
-    nargs='?',
-    type=str
-)
-parser_extract.add_argument("offset",
-    help="(int) extraction offset. use zero for file start. negative values reference from EOF",
-    metavar='offset',
-    nargs=1,
-    type=int
-)
-parser_extract.add_argument("length",
-    help="(int) length of bytes to extract. 0 extracts data from offset to EOF. if negative, returns reversed output (extracts backwards from offset)",
-    metavar='length',
-    nargs=1,
-    type=int
-)
-parser_extract.add_argument("file1",
-    help="primary target input file",
-    nargs=1,
-    type=argparse.FileType('r')
-)
-parser_extract.set_defaults(func=func_extract)
-# find subparser
-parser_find = subparsers.add_parser("find",
-    help="attempts to find separate files inside input file, such as JPG, GIF, PNG, etc."
-)
-parser_find.add_argument("file1",
-    help="primary target input file",
-    nargs=1,
-    type=argparse.FileType('r')
-)
-parser_find.set_defaults(func=func_find)
-# hex subparser
-parser_hexdump = subparsers.add_parser("hexdump",
-  help="output target file into hexadecimal-formatted output"
-)
-parser_hexdump.add_argument("-e", "--encoding",
-  help="encoding for hexdump output. possible values are 'utf-8', 'utf-16', 'latin', 'ebcdic'. default is 'utf-8'",
-  default='utf-8',
-  metavar='encoding',
-  nargs='?',
-  type=str
-)
-parser_hexdump.add_argument("file1",
-  help="primary target input file",
-  nargs=1,
-  type=argparse.FileType('r')
-)
-parser_hexdump.set_defaults(func=func_hexdump)
-# info subparser
-parser_info = subparsers.add_parser("info",
-  help="display detailed information about target and system"
-)
-parser_info.add_argument("-e", "--encoding",
-  help="encoding to use for file. valid options are 'utf-8', 'utf-16', 'latin1', ebcdic'. default is 'utf-8'",
-  default='utf-8',
-  metavar='encoding',
-  nargs='?',
-  type=str
-)
-parser_info.add_argument("file1",
-  help="primary target input file",
-  nargs=1,
-  type=argparse.FileType('r')
-)
-parser_info.set_defaults(func=func_info)
-# output subparser
-parser_output = subparsers.add_parser("output",
-  help="outputs specified byte sequence"
-)
-parser_output.add_argument("output1",
-  help="sequence to output",
-  nargs=1,
-  type=str
-)
-parser_output.add_argument("-e", "--encoding",
-  help="character encoding to use for decoding to bytes. valid options are 'utf-8', 'utf-16', 'latin1', 'ebcdic'. default is 'utf-8'",
-  default='utf-8',
-  metavar='encoding',
-  nargs='?',
-  type=str
-)
-parser_output.set_defaults(func=func_output)
-# reverse subparser
-parser_reverse = subparsers.add_parser("reverse",
-  help="reverse an input"
-)
-parser_reverse.add_argument("file1",
-  help="primary target input file",
-  nargs=1,
-  type=argparse.FileType('r')
-)
-parser_reverse.set_defaults(func=func_reverse)
-# swap subparser
-parser_swap = subparsers.add_parser("swap",
-  help="swap byte order of input (toggles big-/little-endian)"
-)
-parser_swap.add_argument("file1",
-  help="primary target input file",
-  nargs=1,
-  type=argparse.FileType('r')
-)
-parser_swap.set_defaults(func=func_swap)
-# xor subparser
-parser_xor = subparsers.add_parser('xor',
-  help="xor provided targets with one another"
-)
-parser_xor.add_argument("file1",
-  help="primary input file",
-  nargs='?',
-  type=argparse.FileType('r')
-)
-parser_xor.add_argument("file2",
-  help="secondary input file",
-  nargs='?',
-  type=argparse.FileType('r')
-)
-parser_xor.add_argument("-b", "--byte",
-  help="byte sequence to xor with entire input file",
-  default=False,
-  metavar='byte',
-  nargs=1,
-  type=str
-)
-parser_xor.add_argument("-B", "--bytes",
-  help="use provided byte sequences for xor operation instead",
-  default=False,
-  metavar=('byte1', 'byte2'),
-  nargs=2,
-  type=str
-)
-parser_xor.add_argument("-l", "--length",
-  help="xor for specified length. default all",
-  metavar='length',
-  nargs='?',
-  type=int
-)
-parser_xor.add_argument("-o1", "--offset1",
-  help="xor beginning at provided offset in first input file. default 0",
-  default=0,
-  metavar='offset',
-  nargs='?',
-  type=int
-)
-parser_xor.add_argument("-o2", "--offset2",
-  help="xor beginning at provided offset in second input file. default 0",
-  default=0,
-  metavar='offset',
-  nargs='?',
-  type=int
-)
-parser_xor.set_defaults(func=func_xor)
-args = parser.parse_args()
-args.func(args)
+
+def make_parser():
+    """Create ArgumentParser instance with the specs we need.
+    """
+    parser = argparse.ArgumentParser(
+        description="Perform a variety of byte-level operations on files or byte sequences.",
+        prog="optool.py",
+        usage="optool.py"
+    )
+    # here goes [OPTIONS] you want to feed to your command
+    parser.add_argument("--version",
+        action='version',
+        version='optool.py ' + app_version + ' by L0j1k'
+    )
+    #parser.set_defaults(func=main)
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # extract subparser
+    parser_extract = subparsers.add_parser("extract",
+        help="extract a segment of specified length from specified offset in target file"
+    )
+    parser_extract.add_argument("-a", "--address",
+        help="(hex) base hexadecimal address for extraction",
+        const=0,
+        default=0,
+        metavar='hex_address',
+        nargs='?',
+        type=str
+    )
+    parser_extract.add_argument("offset",
+        help="(int) extraction offset. use zero for file start. negative values reference from EOF",
+        metavar='offset',
+        nargs=1,
+        type=int
+    )
+    parser_extract.add_argument("length",
+        help="(int) length of bytes to extract. 0 extracts data from offset to EOF. if negative, returns reversed output (extracts backwards from offset)",
+        metavar='length',
+        nargs=1,
+        type=int
+    )
+    parser_extract.add_argument("file1",
+        help="primary target input file",
+        nargs=1,
+        type=argparse.FileType('r')
+    )
+    parser_extract.set_defaults(func=func_extract)
+
+    # find subparser
+    parser_find = subparsers.add_parser("find",
+        help="attempts to find separate files inside input file, such as JPG, GIF, PNG, etc."
+    )
+    parser_find.add_argument("file1",
+        help="primary target input file",
+        nargs=1,
+        type=argparse.FileType('r')
+    )
+    parser_find.set_defaults(func=func_find)
+
+    # hex subparser
+    parser_hexdump = subparsers.add_parser("hexdump",
+      help="output target file into hexadecimal-formatted output"
+    )
+    parser_hexdump.add_argument("-e", "--encoding",
+      help="encoding for hexdump output. possible values are 'utf-8', 'utf-16', 'latin', 'ebcdic'. default is 'utf-8'",
+      default='utf-8',
+      metavar='encoding',
+      nargs='?',
+      type=str
+    )
+    parser_hexdump.add_argument("file1",
+      help="primary target input file",
+      nargs=1,
+      type=argparse.FileType('r')
+    )
+    parser_hexdump.set_defaults(func=func_hexdump)
+
+    # info subparser
+    parser_info = subparsers.add_parser("info",
+      help="display detailed information about target and system"
+    )
+    parser_info.add_argument("-e", "--encoding",
+      help="encoding to use for file. valid options are 'utf-8', 'utf-16', 'latin1', ebcdic'. default is 'utf-8'",
+      default='utf-8',
+      metavar='encoding',
+      nargs='?',
+      type=str
+    )
+    parser_info.add_argument("file1",
+      help="primary target input file",
+      nargs=1,
+      type=argparse.FileType('r')
+    )
+    parser_info.set_defaults(func=func_info)
+
+    # output subparser
+    parser_output = subparsers.add_parser("output",
+      help="outputs specified byte sequence"
+    )
+    parser_output.add_argument("output1",
+      help="sequence to output",
+      nargs=1,
+      type=str
+    )
+    parser_output.add_argument("-e", "--encoding",
+      help="character encoding to use for decoding to bytes. valid options are 'utf-8', 'utf-16', 'latin1', 'ebcdic'. default is 'utf-8'",
+      default='utf-8',
+      metavar='encoding',
+      nargs='?',
+      type=str
+    )
+    parser_output.set_defaults(func=func_output)
+
+    # reverse subparser
+    parser_reverse = subparsers.add_parser("reverse",
+      help="reverse an input"
+    )
+    parser_reverse.add_argument("file1",
+      help="primary target input file",
+      nargs=1,
+      type=argparse.FileType('r')
+    )
+    parser_reverse.set_defaults(func=func_reverse)
+
+    # swap subparser
+    parser_swap = subparsers.add_parser("swap",
+      help="swap byte order of input (toggles big-/little-endian)"
+    )
+    parser_swap.add_argument("file1",
+      help="primary target input file",
+      nargs=1,
+      type=argparse.FileType('r')
+    )
+    parser_swap.set_defaults(func=func_swap)
+
+    # xor subparser
+    parser_xor = subparsers.add_parser('xor',
+      help="xor provided targets with one another"
+    )
+    parser_xor.add_argument("file1",
+      help="primary input file",
+      nargs='?',
+      type=argparse.FileType('r')
+    )
+    parser_xor.add_argument("file2",
+      help="secondary input file",
+      nargs='?',
+      type=argparse.FileType('r')
+    )
+    parser_xor.add_argument("-b", "--byte",
+      help="byte sequence to xor with entire input file",
+      default=False,
+      metavar='byte',
+      nargs=1,
+      type=str
+    )
+    parser_xor.add_argument("-B", "--bytes",
+      help="use provided byte sequences for xor operation instead",
+      default=False,
+      metavar=('byte1', 'byte2'),
+      nargs=2,
+      type=str
+    )
+    parser_xor.add_argument("-l", "--length",
+      help="xor for specified length. default all",
+      metavar='length',
+      nargs='?',
+      type=int
+    )
+    parser_xor.add_argument("-o1", "--offset1",
+      help="xor beginning at provided offset in first input file. default 0",
+      default=0,
+      metavar='offset',
+      nargs='?',
+      type=int
+    )
+    parser_xor.add_argument("-o2", "--offset2",
+      help="xor beginning at provided offset in second input file. default 0",
+      default=0,
+      metavar='offset',
+      nargs='?',
+      type=int
+    )
+    parser_xor.set_defaults(func=func_xor)
+    return parser
+
+
+def main():
+    """Entry point orchestrating what module does when run as a script.
+    """
+    parser = make_parser()
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
 
 ## argparse subparsers:
 # subparser xor [-o offset] file1 file2 | -b byte1 file1 | file1 -b byte1 | -B byte1 byte2
